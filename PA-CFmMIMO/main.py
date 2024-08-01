4ec487201d3a70a07b3e18ccbe79ddb938e1fa55
HieuND-VN <hieunguyendoan.work@gmail.com>
2024-08-01 22:31:29 +0900

    create_sysmod

--- PA-CFmMIMO/main.py
+++ PA-CFmMIMO/main.py
@@ -1,5 +1,7 @@
 import time
 import argparse
+import torch 
+
 if __name__ == "__main__":
     total_start = time.time()
     parser = argparse.ArgumentParser()
@@ -10,3 +12,13 @@ if __name__ == "__main__":
     parser.add_argument('-h_ap', "--height_AP", type=int, default = 0.015, help="Height of Access Point (in km).")
     parser.add_argument('-h_ue', "--height_UE", type=int, default=0.00165, help="Height of User Equipment (in km).")
     parser.add_argument('-p', "--power_pilot", type=float, default=1, help="Power of pilot signal (in mW)")
+    parser.add_argument('-P', "-power_AP", type=float, default=100, help="Transmition power of each AP")
+
+
+
+    parser.add_argument('-dev', "--device", type=str, default="cuda", choices=["cpu", "cuda"])
+
+    args = parser.parse_args()
+    if args.device == "cuda" and not torch.cuda.is_available():
+        print("\ncuda is not avaiable.\n")
+        args.device = "cpu"
