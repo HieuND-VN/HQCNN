e0f08356a46497110175f2c23d2e225859288be3
HieuND-VN <121997119+HieuND-VN@users.noreply.github.com>
2024-08-03 15:45:25 +0900

    030824

--- PA-CFmMIMO/SystemModel.py
+++ PA-CFmMIMO/SystemModel.py
@@ -25,36 +25,8 @@ class SysMod():
         sensitivity_2 = 0.2
         AP_position = np.zeros((self.num_ap,1), dtype = "complex")
         theta = np.linsapce(0,2*np.pi,self.num_ap)
+        for i in range(self.num_ap):
+            AP_position[i] = self.length * sensitivity_1 * np.cos(theta[i]) + 1j * self.length * sensitivity_2 * np.sin(theta[i])
         return AP_position
 
-    
-    def distance_calculator(self):
-        distance = np.zeros((self.num_ap, self.num_ue))
-        return distance
-    
-    def pathloss_calculator(self):
-        pathloss = np.zeros((self.num_ap, self.num_ue))
-        return pathloss
-    
-    def LSF_calculator(self):
-        beta = np.zeros((self.num_ap,self.num_ue))
-        return beta
-    
-    # Calculate gamma coefficient
-    def gamma_calculator(self):
-        gamma = np.zeros((self.num_ap,self.num_ue))
-        return gamma
-    
-    def channel_coefficient_calculator(self):
-        channel_coeff = np.zeros((self.num_ap,self.num_ue))
-        return channel_coeff
-    
-    # Calculate SINR by calculate numerator and deminorator
-    def SINR_calculator(self, pilot_index):
-        sinr = np.zeros(self.num_ue)
-        return sinr
-    
-    def dl_rate_calculator(self, pilot_index):
-        sinr = self.SINR_calculator(pilot_index)
-        dl_rate = np.log2(1+sinr)
-        return dl_rate
+    def dataset_generator(self):
